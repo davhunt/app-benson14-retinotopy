cbada414084d3263c31e8a80d7ceae0ce42ca077
Brad Caron <bacaron@iu.edu>
2019-11-05 15:33:47 -0500

    Update run.sh
    Move surfaces into actual prf output

--- run.sh
+++ run.sh
@@ -22,8 +22,8 @@ do
 done
 
 echo "organizing output"
-mkdir -p prf varea benson14_surfaces
-mv copy/surf/*benson14* benson14_surfaces/ 
+mkdir -p prf varea prf/benson14_surfaces
+mv copy/surf/*benson14* prf/benson14_surfaces/ 
 mv copy/mri/*benson14* prf
 
 mv prf/benson14_eccen.nii.gz prf/eccentricity.nii.gz 
