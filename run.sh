9e6b34f2b5de8ade28b3d7932fbab83733dae9c8
David Hunt <davhunt@indiana.edu>
2020-09-28 12:58:34 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -6,25 +6,27 @@ set -x
 set -e
 
 echo "copying input freesurfer to here because benson14_retinotopy writes output to freesurfer dir"
-cp -r $fsdir copy
+cp -r $fsdir fs_copy && chmod +w fs_copy
 
 echo "delete any old retinotopy"
-rm -rf copy/mri/*benson14*
-rm -rf copy/surf/*benson14*
+rm -rf fs_copy/mri/*benson14*
+rm -rf fs_copy/surf/*benson14*
 
 echo "running benson14_retinotopy"
-time python -m neuropythy benson14_retinotopy copy -d .
+time python -m neuropythy benson14_retinotopy fs_copy -d .
 
-echo "converting output to nifti"
-for i in copy/mri/*benson14*
+echo "converting mgz to nifti"
+for i in fs_copy/mri/*benson14*
 do 
 	mri_convert $i ${i%.*}.nii.gz
 done
 
 echo "organizing output"
-mkdir -p prf varea prf/prf_surfaces varea_surf
-mv copy/surf/*benson14* prf/prf_surfaces/ 
-mv copy/mri/*benson14*.nii.gz prf
+mkdir -p prf prf/prf_surfaces varea varea_surf
+cp key.txt label.json varea && cp key.txt label.json varea_surf
+
+mv fs_copy/surf/*benson14* prf/prf_surfaces
+mv fs_copy/mri/*benson14*.nii.gz prf
 
 mv prf/benson14_eccen.nii.gz prf/eccentricity.nii.gz 
 mv prf/benson14_sigma.nii.gz prf/rfWidth.nii.gz 
@@ -44,6 +46,7 @@ done
 echo "running create_R2"
 # this will create a binary mask R2 of 1's and 0's
 # based on whether a voxel is assigned a visual area or not
+# Just for visualization purposes, not actual r^2 values
 ./create_R2.py ./varea/parc.nii.gz ./prf/prf_surfaces/rh.varea ./prf/prf_surfaces/lh.varea
 
 echo "saving surfaces in .mat file"
